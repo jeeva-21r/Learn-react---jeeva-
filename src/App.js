0dfa1aecbeccb2821da2ae79fca653fa375d01a9
jeeva-21r <jeevanantham21r@gmail.com>
2023-05-20 01:19:48 +0530

    first change

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h>Hello nanba we are going to learn about react JS</h> 
+          <h1>need to concentrate more in this </h1><code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
